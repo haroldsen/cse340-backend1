d1dbe7e651481ceb7b98d3edd9baf7e827b59659
Christian Haroldsen <ctharoldsen@gmail.com>
2026-01-14 09:44:05 -0700

    Made progress on introToRouting.

---
+++ introToRouting/server.js
@@ -0,0 +1,35 @@
+// Imports
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Declare Important Variables
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Setup Express Server
+const app = express();
+
+/* Configure Express middleware */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Declare Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+});
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+});
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+});
+
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
